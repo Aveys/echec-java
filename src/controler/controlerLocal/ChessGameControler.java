fc382f95d13aa53dfeb5d066214c6e901a5b2d4a
Arthur Veys <veysarthur@gmail.com>
2015-11-02 10:45:38 +0100

    add class

---
+++ src/controler/controlerLocal/ChessGameControler.java
@@ -0,0 +1,7 @@
+package controler.controlerLocal;
+
+/**
+ * Created by arthurveys on 02/11/15 for ProjetDP2.
+ */
+public class ChessGameControler {
+}
---
+++ src/vue/ChessGameIHM.java
@@ -0,0 +1,7 @@
+package vue;
+
+/**
+ * Created by arthurveys on 02/11/15 for ProjetDP2.
+ */
+public class ChessGameIHM {
+}

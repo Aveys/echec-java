87fc3cdb0dfe64abaddfa43e4319c15cc76acfdf
lblain69 <lblain69@gmail.com>
2015-11-02 11:05:52 +0100

    ChessGame implementation

--- src/model/ChessGame.java
+++ src/model/ChessGame.java
@@ -1,7 +1,47 @@
 package model;
 
+import java.util.Observable;
+
 /**
  * Created by arthurveys on 02/11/15 for ProjetDP2.
  */
-public class ChessGame {
+public class ChessGame extends Observable{
+
+    private Echiquier echiquier;
+
+    public ChessGame(){
+
+        this.echiquier = new Echiquier();
+    }
+
+    public boolean move (int xInit, int yInit, int xFinal, int yFinal){
+
+        boolean moved = false;
+
+        if(echiquier.isMoveOk(xInit,yInit,xFinal,yFinal)){
+            moved = echiquier.move(xInit,yInit,xFinal,yFinal);
+        }
+        else{
+            System.out.println("Mouvement impossible");
+        }
+        return moved;
+    }
+
+    public boolean isEnd(){
+        return echiquier.isEnd();
+    }
+
+    public String getMessage(){
+        return echiquier.getMessage();
+    }
+
+    public Couleur getColorCurrentPlayer(){
+        return echiquier.getColorCurrentPlayer();
+    }
+
+    public String toString(){
+        return echiquier.toString();
+    }
+
+
 }

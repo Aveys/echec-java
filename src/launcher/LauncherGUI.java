6057d8a2082aa5f93cabd573a0e3c8cbbd067862
Arthur Veys <veysarthur@gmail.com>
2015-11-02 14:55:56 +0100

    exit on close

--- src/launcher/LauncherGUI.java
+++ src/launcher/LauncherGUI.java
@@ -23,7 +23,7 @@ public class LauncherGUI {
         chessGameGUI = new ChessGameGUI();
         chessGame.addObserver(chessGameGUI);
 
-        chessGameGUI.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        chessGameGUI.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         chessGameGUI.pack();
         chessGameGUI.setResizable(true);
         chessGameGUI.setLocationRelativeTo( null );

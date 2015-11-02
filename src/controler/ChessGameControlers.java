3075ff48664b857008a1b5fec7d82feaa4ef5e43
Arthur Veys <veysarthur@gmail.com>
2015-11-02 11:08:21 +0100

    modify controlers

--- src/controler/ChessGameControlers.java
+++ src/controler/ChessGameControlers.java
@@ -8,8 +8,29 @@ import model.Couleur;
  */
 public interface ChessGameControlers {
 
+	/**
+	 * Move a piece on the board
+	 * @param initCoord initial coord
+	 * @param finalCoord final coord
+	 * @return True if movement correct and executed, false if failed
+	 */
 	public boolean move(Coord initCoord, Coord finalCoord);
+
+	/**
+	 * Return message from the move
+	 * @return
+	 */
 	public String getMessage();
+
+	/**
+	 * check if game is finished
+	 * @return true if game is finished, false otherwise
+	 */
 	public boolean isEnd();
+
+	/**
+	 * return the color of the current player
+	 * @return The color of the current player
+	 */
 	public Couleur getColorCurrentPlayer();
 }
--- src/controler/controlerLocal/ChessGameControler.java
+++ src/controler/controlerLocal/ChessGameControler.java
@@ -1,7 +1,36 @@
 package controler.controlerLocal;
 
+import controler.ChessGameControlers;
+import model.ChessGame;
+import model.Coord;
+import model.Couleur;
+
 /**
  * Created by arthurveys on 02/11/15 for ProjetDP2.
  */
-public class ChessGameControler {
+
+
+public class ChessGameControler implements ChessGameControlers{
+
+	public ChessGame game = new ChessGame();
+
+	@Override
+	public boolean move(Coord initCoord, Coord finalCoord) {
+		return game.move(initCoord.x,initCoord.y,finalCoord.x,finalCoord.y);
+	}
+
+	@Override
+	public String getMessage() {
+		return game.getMessage();
+	}
+
+	@Override
+	public boolean isEnd() {
+		return game.isEnd();
+	}
+
+	@Override
+	public Couleur getColorCurrentPlayer() {
+		return game.getColorCurrentPlayer();
+	}
 }
